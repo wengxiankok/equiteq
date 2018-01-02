f5b784e5188c71fbbf29e1a6720ac7216cce309a
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-02 16:41:42 +0800

    add production conditional

--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,8 +1,12 @@
 import gulp    from 'gulp';
 import webpack from 'webpack-stream';
+import UglifyJSPlugin from 'uglifyjs-webpack-plugin';
 
 // import config
 import configs from './../_config';
+
+// import utilities
+import argv from './../utils/arguments';
 
 // create basic array of entry files
 let entries = [];
@@ -11,6 +15,12 @@ for( const path in configs.webpack.entry ) entries.push( configs.webpack.entry[p
 const destination = `${configs.paths.build}/${configs.folders.javascript}`;
 
 function scripts(){
+  if( argv.env === 'production' ){
+    configs.webpack.plugins = [
+      new UglifyJSPlugin()
+    ];
+  }
+
   return gulp.src( entries )
   .pipe( webpack(configs.webpack) )
   .pipe( gulp.dest(destination) );
