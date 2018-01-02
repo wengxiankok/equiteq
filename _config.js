524aa5f5fb4b315638be1426666b495d223192cb
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-02 11:53:08 +0800

    create config

---
+++ _config.js
@@ -0,0 +1,69 @@
+const folders = {
+  sass: 'sass',
+  stylesheets: 'css',
+  javascript: 'js',
+  images: 'img'
+};
+
+const paths = {
+  source: './source',
+  build: './build',
+  dist: './dist'
+};
+
+const sass = {
+
+};
+
+const webpack = {
+  watch: true,
+  entry: {
+    app: `./${paths.source}/${folders.javascript}/index.js`,
+    vendor: `./${paths.source}/${folders.javascript}/vendor.js`
+  },
+  output: {
+    filename: '[name].js'
+  },
+  stats: {
+    version: false,
+    timings: true
+  },
+  module: {
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+    ]
+  }
+};
+
+const browsersync = {
+  logLevel: 'info',
+  logFileChanges: false,
+  server: {
+    baseDir: paths.build,
+    port: 3000
+  },
+  files: [
+    `${paths.build}/${folders.stylesheets}/*.css`,
+    `${paths.build}/${folders.javascript}/*.js`,
+    `${paths.build}/*.html`,
+  ]
+};
+
+const imagemin = {
+  options: {
+    verbose: true
+  },
+  plugins: {
+    mozjpeg: {
+      quality: 70
+    },
+    pngquant: {
+      quality: 60
+    },
+    svgo: {}
+  }
+};
+
+const configs  = {folders, paths, sass, webpack, browsersync, imagemin};
+
+export default configs;
