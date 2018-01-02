68b87d2539f6d864e1bb7424c97b3a780a383758
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-02 14:17:54 +0800

    implement argument parser

--- tasks/page.js
+++ tasks/page.js
@@ -1,21 +1,14 @@
 import gulp  from 'gulp';
 import strip from 'gulp-strip-comments';
-import yargs from 'yargs';
-import chalk from 'chalk';
-import log from 'fancy-log';
 
 // import config
 import configs from './../_config';
+import argv from './../utils/arguments';
 
-const argv = yargs.argv;
-
-const ext         = argv.php ? 'php' : 'html';
-const entry       = `${configs.paths.source}/**/*.${ext}`;
+const entry       = `${configs.paths.source}/**/*.${argv().ext}`;
 const destination = `${configs.paths.build}`;
 
 function page(){
-  log(chalk.green(`Editing file type: [${chalk.cyan.bold(ext)}]`));
-
   return gulp.src(entry)
     .pipe( strip() )
     .pipe( gulp.dest(destination) );
