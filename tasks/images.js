84d5d03854aec2e2ece365496a2b76dd7c492ad8
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-02 14:23:29 +0800

    create image optimizer task

---
+++ tasks/images.js
@@ -0,0 +1,25 @@
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+import mozjpeg from 'imagemin-mozjpeg';
+import pngquant from 'imagemin-pngquant';
+import svgo from 'imagemin-svgo';
+
+// import config
+import configs from './../_config';
+
+const plugins = [
+  mozjpeg( configs.imagemin.plugins.mozjpeg ),
+  pngquant( configs.imagemin.plugins.pngquant ),
+  svgo( configs.imagemin.plugins.svgo )
+];
+
+const entry = `${configs.paths.source}/${configs.folders.images}/**/*`;
+const destination = `${configs.paths.build}/${configs.folders.images}`;
+
+function images(){
+  return gulp.src( entry )
+    .pipe( imagemin( plugins, configs.imagemin.options ) )
+    .pipe( gulp.dest(destination) );
+}
+
+export default images;
