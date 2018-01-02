4966e628781e79a3c1fdabf968e2ba02ae676ea8
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-02 16:42:23 +0800

    add quick skip

--- tasks/delete.js
+++ tasks/delete.js
@@ -4,10 +4,21 @@ import chalk from 'chalk';
 
 // import config
 import configs from './../_config';
+// import utilities
+import argv  from './../utils/arguments';
 
-const entry = `${configs.paths.build}/**/*`;
+let entry = [
+  `${configs.paths.build}/**/*`,
+  `!${configs.paths.build}/img`,
+  `!${configs.paths.build}/img/**`
+];
 
-function clean(){
+function clean(done){
+  if( argv.quick ){
+    log( chalk.red(`Ignored '${chalk.cyan('img')}' folder.`));
+    return done();
+  }
+
   return del( entry )
     .then(log( chalk.red.bold('Folder cleaned.') ));
 }
