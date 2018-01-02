745d72337a74788a42b8b5d8eae521db4129a021
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-02 17:55:20 +0800

    add copy task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,4 @@
 import gulp  from 'gulp';
-import chalk from 'chalk';
-import log   from 'fancy-log';
 
 // import tasks
 import watcher from './tasks/watcher';
@@ -10,18 +8,18 @@ import scripts from './tasks/scripts';
 import styles  from './tasks/styles';
 import images  from './tasks/images';
 import clean   from './tasks/delete';
+import copy   from './tasks/copy';
 
 // import utilities
 import configs from './_config';
-import argv    from './utils/arguments';
 
 // specific tasks
-const tasks = [ page, images, scripts, styles, clean ];
+const tasks = [ page, images, scripts, styles, clean, copy ];
 tasks.forEach( task => gulp.task(task) );
 
 // main tasks
-gulp.task('dev', gulp.series( clean, gulp.parallel(server, watcher, page, styles, scripts, images) ));
-gulp.task('build', gulp.series( [clean, set], gulp.parallel(page, scripts, styles, images) ));
+gulp.task('dev', gulp.series( clean, gulp.parallel(server, watcher, page, styles, scripts, images, copy) ));
+gulp.task('build', gulp.series( [clean, set], gulp.parallel(page, scripts, styles, images, copy) ));
 
 function set(done){
   configs.webpack.watch = false;
