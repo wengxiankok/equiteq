d873647cd9a5c2af4b4930321b9faa484e7c8d96
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-02 17:24:29 +0800

    add image optimization conditional

--- tasks/images.js
+++ tasks/images.js
@@ -3,9 +3,12 @@ import imagemin from 'gulp-imagemin';
 import mozjpeg  from 'imagemin-mozjpeg';
 import pngquant from 'imagemin-pngquant';
 import svgo     from 'imagemin-svgo';
+import log      from 'fancy-log';
+import chalk    from 'chalk';
 
 // import utilities
 import configs from './../_config';
+import argv    from './../utils/arguments';
 
 const plugins = [
   mozjpeg( configs.imagemin.plugins.mozjpeg ),
@@ -16,7 +19,12 @@ const plugins = [
 const entry = `${configs.paths.source}/${configs.folders.images}/**/*`;
 const destination = `${configs.paths.build}/${configs.folders.images}`;
 
-function images(){
+function images(done){
+  if( argv.quick ){
+    log( chalk.red(`Ignored '${chalk.cyan('img')}' folder.`));
+    return done();
+  }
+  
   return gulp.src( entry )
     .pipe( imagemin( plugins, configs.imagemin.options ) )
     .pipe( gulp.dest(destination) );
