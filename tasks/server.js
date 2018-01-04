281eaff4c3586b5e0595ab8542f20a013551b487
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-04 16:43:35 +0800

    simplify folder check

--- tasks/server.js
+++ tasks/server.js
@@ -1,18 +1,14 @@
 import browsersync from 'browser-sync';
 
 // import utilities
-import {paths, folders} from './configs/_configs';
+import {paths} from './configs/_configs';
 
 const config = {
   logLevel: 'info',
   logFileChanges: false,
   server: paths.build,
   // proxy: 'website.local',
-  files: [
-    `${paths.build}/${folders.stylesheets}/*.css`,
-    `${paths.build}/${folders.javascript}/*.js`,
-    `${paths.build}/*.html`,
-  ]
+  files: `${paths.build}/**/*.*`
 };
 
 export default function server(){
