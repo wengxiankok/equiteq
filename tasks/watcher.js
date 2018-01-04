eaad1c236857a4fe00e4213a2fad355cbdd2898c
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-04 16:43:03 +0800

    add build folder deleter

--- tasks/watcher.js
+++ tasks/watcher.js
@@ -1,31 +1,42 @@
 import gulp  from 'gulp';
 import chalk from 'chalk';
 import log   from 'fancy-log';
+import del from 'del';
 
 // import tasks
 import page   from './page';
 import styles from './styles';
 
 // import config
-import {paths} from './configs/_configs';
+import {paths, folders} from './configs/_configs';
+import argv    from './utils/arguments';
 
 function watcher(){
-  const styleWatcher  = gulp.watch(`${paths.source}/**/*.scss`);
-  const scriptWatcher = gulp.watch(`${paths.source}/**/*.js`);
-  const pageWatcher   = gulp.watch(`${paths.source}/**/*.html`);
+  const watchers = {
+    scripts: gulp.watch(`${paths.source}/${folders.javascript}/**/*.js`),
+    styles: gulp.watch(`${paths.source}/${folders.sass}/**/*.scss`, styles),
+    page: gulp.watch(`${paths.source}/*.${argv.pageExt}`, page)
+  };
 
-  watchHandler( styleWatcher, styles );
-  watchHandler( scriptWatcher, null );
-  watchHandler( pageWatcher, page );
+  for( const task in watchers ) watchHandler( watchers[task], task );
 }
 
-function watchHandler( watcher, task ){
-  watcher.on('all', (events, path) =>{
+function watchHandler( watcher ){
+  watcher.on('all', (events, path)=>{
+    if( events === 'unlink' ) return;
+
     const filePath = path.split(/\/|\\/).pop();
     log( chalk.gray('---------------------') );
     log( `Detected ${ chalk.green.bold(events)} on ${ chalk.yellow.bold(filePath)}.` );
-    log( `Executing task '${chalk.cyan(task.name)}'.`);
-    if( task != null ) task();
+  });
+
+  watcher.on('unlink', path=>{
+    const re = new RegExp( paths.source.split('/').pop() );
+    const buildPath = path.replace( re, paths.build.replace('/', '\\') );
+
+    log( chalk.red.bold(`'${ chalk.yellow(buildPath.split(/\/|\\/).pop()) }' deleted.`) );
+    del(buildPath);
   });
 }
+
 export default watcher;
