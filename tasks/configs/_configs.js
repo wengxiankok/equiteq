56ff8b6125a74613b6ea7b59c54e32dfaec14b0a
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-01-05 16:33:29 +0800

    base page ignore on pageExt

--- tasks/configs/_configs.js
+++ tasks/configs/_configs.js
@@ -11,21 +11,4 @@ const paths = {
   dist: './dist'
 };
 
-const ignores = [
-  `${paths.source}/**/*`,
-  // ignore HTML files
-  `!${paths.source}/**/*.html`,
-  // ignore PHP files
-  `!${paths.source}/**/*.php`,
-  // ignore images
-  `!${paths.source}/${folders.images}`,
-  `!${paths.source}/${folders.images}/**/*`,
-  //ignore sass stylesheets
-  `!${paths.source}/${folders.sass}`,
-  `!${paths.source}/${folders.sass}/**/*`,
-  // ignore Javascript files
-  `!${paths.source}/${folders.javascript}`,
-  `!${paths.source}/${folders.javascript}/**/*`,
-];
-
-export {paths, folders, ignores};
+export {paths, folders};
--- tasks/copy.js
+++ tasks/copy.js
@@ -1,13 +1,28 @@
 import gulp from 'gulp';
 
 // import utilities
-import {paths, ignores} from './configs/_configs';
+import {paths, folders} from './configs/_configs';
+import argv from './utils/arguments';
 
-const entry = ignores;
+const ignores = [
+  `${paths.source}/**/*`,
+  // ignore page files
+  `!${paths.source}/**/*.${argv.pageExt}`,
+  // ignore images
+  `!${paths.source}/${folders.images}`,
+  `!${paths.source}/${folders.images}/**/*`,
+  //ignore sass stylesheets
+  `!${paths.source}/${folders.sass}`,
+  `!${paths.source}/${folders.sass}/**/*`,
+  // ignore Javascript files
+  `!${paths.source}/${folders.javascript}`,
+  `!${paths.source}/${folders.javascript}/**/*`,
+];
+
 const dest  = `${paths.build}`;
 
 function copy(){
-  return gulp.src( entry )
+  return gulp.src( ignores )
     .pipe( gulp.dest(dest) );
 }
 
