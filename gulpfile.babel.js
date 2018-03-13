faa62d30d4e77b16284396f3c52b5e9cf0548d9a
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-03-13 10:14:34 +0800

    reset everything

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,2 @@
-import gulp  from 'gulp';
 
-// import tasks
-import watcher from './tasks/watcher';
-import server  from './tasks/server';
-import page    from './tasks/page';
-import scripts from './tasks/scripts';
-import styles  from './tasks/styles';
-import images  from './tasks/images';
-import clean   from './tasks/delete';
-import copy    from './tasks/copy';
 
-// import utilities
-import wpConfig from './tasks/configs/webpack.config';
-
-// specific tasks
-const tasks = [ page, images, scripts, styles, clean, copy ];
-tasks.forEach( task => gulp.task(task) );
-
-// main tasks
-gulp.task('dev', gulp.series( clean, gulp.parallel( page, styles, images, copy ), gulp.parallel(scripts, server, watcher) ));
-gulp.task('build', gulp.series( [clean, set], gulp.parallel(page, scripts, styles, images, copy) ));
-
-function set(done){
-  wpConfig.watch = false;
-  done();
-}
-
-gulp.task(page);
--- tasks/arguments.js
+++ tasks/arguments.js
@@ -1,8 +1,4 @@
-import yargs from 'yargs';
-import chalk from 'chalk';
-
-const argvs = yargs.argv;
-const ASCII = String.raw`
+const BoilrASCII = String.raw`
   ____        _ _
  |  _ \      (_) |
  | |_) | ___  _| |_ __
@@ -11,23 +7,3 @@ const ASCII = String.raw`
  |____/ \___/|_|_|_|  v0.4.1
 
 `;
-
-function argvParser(){
-  const args = {
-    env: argvs.production || argvs.p ? 'production' : 'develop',
-    pageExt: argvs.php ? 'php' : argvs.type ? argvs.type : 'html',
-    quick: argvs.quick || argvs.q
-  };
-
-  console.log( chalk.gray('---------------------') );
-  console.log( chalk.green(ASCII));
-  console.log(chalk.green(`Current Environment: '${chalk.cyan.bold(args.env)}'`));
-  console.log(chalk.green(`Editing File Type: [.${chalk.cyan.bold(args.pageExt)}]`));
-  if( args.quick ) console.log( chalk.yellow('Skipping Images...'));
-  console.log( chalk.gray('---------------------') );
-
-  return args;
-}
-
-const argv = argvParser();
-export default argv;
