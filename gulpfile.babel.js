6442ab38908ffa685e47e60ba43157259e448d39
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-03-14 18:19:06 +0800

    add clean task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,6 +4,7 @@ import {images} from './tasks/images';
 import {scripts} from './tasks/scripts';
 import {pages} from './tasks/pages';
 import {server} from './tasks/server';
+import {clean} from './tasks/clean';
 import {watcher} from './tasks/watcher';
 
 gulp.task(styles);
@@ -11,6 +12,7 @@ gulp.task(images);
 gulp.task(pages);
 gulp.task(scripts);
 gulp.task(server);
+gulp.task(clean);
 gulp.task(watcher);
 
 gulp.task('dev', gulp.series( styles, watcher ));
