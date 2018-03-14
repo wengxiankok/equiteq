9faed0c1fd13c1711d7c9f64f74e9eb14fdb1c65
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-03-14 18:19:18 +0800

    add './' to paths

--- tasks/configs.js
+++ tasks/configs.js
@@ -4,9 +4,9 @@ export const env = yargs.argv.p ? 'production' : 'development';
 export const production = env === 'production' ? true : false;
 
 export const baseDir = {
-  static: 'static/',
+  static: './static',
   src: './src',
-  dist: 'dist/',
+  dist: './dist',
 
   styles: {
     src: 'sass/**/*.scss',
