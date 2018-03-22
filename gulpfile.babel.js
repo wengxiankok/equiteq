63d1772c32d85a916314904208290a9088872c0a
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-03-22 16:51:35 +0800

    create base structure

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,12 +7,16 @@ import server  from './tasks/server';
 import clean   from './tasks/clean';
 import watcher from './tasks/watcher';
 
-gulp.task(styles);
-gulp.task(images);
-gulp.task(pages);
-gulp.task(scripts);
-gulp.task(server);
-gulp.task(clean);
-gulp.task(watcher);
+/* ---- Import Configs ------------ */
 
-gulp.task('dev', gulp.series( styles, watcher ));
+/* ---- Main Tasks ---------------- */
+gulp.task('dev');
+
+/* ---- Sub Tasks ----------------- */
+gulp.task('styles', gulp.series('clean-css', styles));
+
+/* ---- Private Tasks ------------- */
+gulp.task('clean-all', clean.all);
+gulp.task('clean-images', clean.images);
+gulp.task('clean-css', clean.css);
+gulp.task('clean-js', clean.js);
