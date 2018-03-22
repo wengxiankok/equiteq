b7886fd1d998a0aad3023f44d0647c60399c6168
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-03-22 16:50:51 +0800

    extend clean to support individual folders

--- tasks/clean.js
+++ tasks/clean.js
@@ -1,15 +1,25 @@
 /* ---- Import NPM Modules --------- */
-import del       from 'del';
+import del         from 'del';
 
 /* ---- Import Configs ------------ */
-import {baseDir} from './configs';
-import {log}     from './utils';
+import {baseDir}   from './configs';
+import {log, path} from './utils';
 
-const source = baseDir.dist;
+class Clean{
+  constructor(){
+    this.all    = () => this.delete(baseDir.dist);
+    this.css    = () => this.delete(path.join(baseDir.dist, baseDir.styles.dest));
+    this.js     = () => this.delete(path.join(baseDir.dist, baseDir.scripts.dest));
+    this.images = () => this.delete(path.join(baseDir.dist, baseDir.images.dest));
+  }
 
-export default function clean(){
-  return del(source)
-    .then(()=>{
-      log.print('Dist folder cleaned.', 'red');
-    });
+  delete(toDelete = baseDir.dist){
+    return del(toDelete)
+      .then(()=>{
+        log.print(`${log.string(toDelete, 'yellow')} folder cleaned.`, 'red');
+      });
+  }
 }
+
+const clean = new Clean();
+export default clean;
