4a47da7c5cf9cb64bffab096abaa605370722673
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-03-23 10:22:31 +0800

    implement path util

--- tasks/server.js
+++ tasks/server.js
@@ -1,12 +1,9 @@
-/* ---- Import Node.js Fuctions ---- */
-import path         from 'path';
-
 /* ---- Import NPM Modules --------- */
-import browsersync from 'browser-sync';
+import browsersync        from 'browser-sync';
 
 /* ---- Import Configs ------------ */
 import {baseDir, configs} from './configs';
-// import {size} from './utils';
+import {path}             from './utils';
 
 // Define File Paths
 const config = configs.browsersync;
@@ -17,6 +14,7 @@ const options = {
   files: watchFiles
 };
 
+// merge options
 for(const opt in config.options){
   options[opt] = config.options[opt];
 }
@@ -25,8 +23,8 @@ for(const opt in config.options){
 for(const glob in config.watch){
   const g = config.watch[glob];
 
-  if(typeof g === 'object') g.forEach(i => watchFiles.push(path.posix.join(baseDir.dist, i)));
-  else watchFiles.push(path.posix.join(baseDir.dist, g));
+  if(typeof g === 'object') g.forEach(i => watchFiles.push(path.join(baseDir.dist, i)));
+  else watchFiles.push(path.join(baseDir.dist, g));
 }
 
 // create proxy or static server
