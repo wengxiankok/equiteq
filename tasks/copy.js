bf51ae7c18ecfa75219ff26d2e8c6caf68caf2da
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-03-23 11:37:44 +0800

    add size logger

--- tasks/copy.js
+++ tasks/copy.js
@@ -3,6 +3,7 @@ import gulp from 'gulp';
 
 /* ---- Import Configs ------------ */
 import {baseDir} from './configs';
+import {size} from './utils';
 
 // Define File Paths
 const source = `${baseDir.static}/**/*.*`;
@@ -10,5 +11,6 @@ const dest   = baseDir.dist;
 
 export default function copy(){
   return gulp.src(source)
+    .pipe(size())
     .pipe(gulp.dest(dest));
 }
