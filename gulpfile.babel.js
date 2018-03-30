bf9d39e11f546f6654d08d9c60cd40b9afad391e
Bryon Sigawin <bryon.sigawin@forefront.com.my>
2018-03-30 10:39:52 +0800

    update tasks

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -14,19 +14,19 @@ import watcher from './tasks/watcher';
 /* ---- Import Configs ------------ */
 
 /* ---- Private Tasks ------------- */
-gulp.task(scripts);
-gulp.task(deploy);
-gulp.task(images);
-gulp.task(styles);
-gulp.task(pages);
-gulp.task(clean);
-gulp.task(copy);
-
 gulp.task('clean-all', clean.all);
 gulp.task('clean-images', clean.images);
 gulp.task('clean-pages', clean.pages);
 gulp.task('clean-css', clean.css);
 gulp.task('clean-js', clean.js);
+
+gulp.task(scripts);
+gulp.task(deploy);
+gulp.task(styles);
+gulp.task(clean);
+gulp.task(copy);
+gulp.task('images', gulp.series('clean-images', images));
+gulp.task('pages', gulp.series('clean-pages', pages));
 
 /* ---- Sub Tasks ----------------- */
 gulp.task('styles', gulp.series('clean-css', styles));
