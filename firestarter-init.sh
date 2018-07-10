937e8c04b0de13dc24daa00a8b51f20b17a77d24
James Ooi <wengteikooi@gmail.com>
2018-07-10 10:33:49 +0800

    Update firestarter-init script

--- firestarter-init.sh
+++ firestarter-init.sh
@@ -6,5 +6,14 @@
 # Get current site name
 site=${PWD##*/}
 
+# Call composer install if wp-core hasn't been downloaded
+if [ ! -e www/wp-core ]; then
+  if ! type "composer" > /dev/null; then
+    printf "\033[91m\033[1m%s:\033[39m\033[0m Please install 'composer' first.\n" "Error" $1
+  else
+    composer install
+  fi
+fi
+
 # Call the init script
 vagrant ssh -c "init-wordpress-site $site"
