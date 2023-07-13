2f132f92b37112e5f0c6d0ca3cb1fd04251d0551
Cloud Ng <cloud.ng@forefront.com.my>
2023-07-13 15:43:44 +0800

    Adding gulp webp support

---
+++ source/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Boilr | A modern front-end boilerplate to kickstart your next project</title>
+
+    <link rel="stylesheet" href="<?= get_bloginfo('template_url') ?>/css/main.css">
+</head>
+
+<body>
+    <h1>A modern front-end boilerplate to kickstart your next project.</h1>
+    <a href="/page2.html">Page 2</a>
+
+    <script src="<?= get_bloginfo('template_url') ?>/js/app.js" charset="utf-8"></script>
+</body>
+
+</html>
--- tasks/images.js
+++ tasks/images.js
@@ -1,40 +1,41 @@
 /* ---- Import Gulp Modules -------- */
-import gulp from 'gulp';
-import newer from 'gulp-newer';
+import gulp from 'gulp'
+import newer from 'gulp-newer'
+import webp from 'gulp-webp'
 
-import imagemin from 'gulp-imagemin';
-import mozjpeg from 'imagemin-mozjpeg';
-import pngquant from 'imagemin-pngquant';
-import svgo from 'imagemin-svgo';
+import imagemin from 'gulp-imagemin'
+import mozjpeg from 'imagemin-mozjpeg'
+import pngquant from 'imagemin-pngquant'
+import svgo from 'imagemin-svgo'
 
 /* ---- Import Configs ------------ */
-import { production, baseDir, configs } from './configs';
-import { log, path, size } from './utils';
+import { production, baseDir, configs } from './configs'
+import { log, path, size } from './utils'
 
 // Define File Paths
-const config = configs.images;
-const source = path.join(baseDir.src, baseDir.images.src);
-const dest = path.join(baseDir.dist, baseDir.images.dest);
+const config = configs.images
+const source = path.join(baseDir.src, baseDir.images.src)
+const dest = path.join(baseDir.dist, baseDir.images.dest)
 
-const plugins = [
-  mozjpeg({ quality: config.quality.jpeg }),
-  pngquant({ quality: config.quality.png }),
-  svgo(),
-];
+const plugins = [mozjpeg({ quality: config.quality.jpeg }), pngquant({ quality: config.quality.png }), svgo()]
 
 export default function images() {
   if (!production) {
-    log.print('Skipping image optimization...', 'red');
+    log.print('Skipping image optimization...', 'red')
     return gulp
       .src(source)
       .pipe(newer(dest))
       .pipe(size())
-      .pipe(gulp.dest(dest));
+      .pipe(gulp.dest(dest))
+      .pipe(webp())
+      .pipe(gulp.dest(dest))
   }
 
   return gulp
     .src(source)
     .pipe(newer(dest))
     .pipe(imagemin(plugins, config.options))
-    .pipe(gulp.dest(dest));
+    .pipe(gulp.dest(dest))
+    .pipe(webp())
+    .pipe(gulp.dest(dest))
 }
